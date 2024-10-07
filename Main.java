c1d051dd60ff22ed1190fccc6b764c836213a6f5
barhat <barhatbekuulubaktiyar@gmail.com>
2024-10-07 10:28:09 +0600

    modified: Main.java, added a method to ask the user if they want to specify the location of cargo boxes, and, if so, make them

--- Main.java
+++ Main.java
@@ -7,23 +7,44 @@ public class Main {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int[] cargoLocation = {0, 0, 311, 0, 17, 385, 0};
+            System.out.println("Do you want to enter cargo box locations by hand?"
+                    + "If so, please enter 1, otherwise 0");
+            int flag = sc.nextInt();
+            if (flag == 1)
+                newCargoLocation(cargoLocation);
 
             ArrayList<Integer> ans = new ArrayList<>();
             int[] martiansInput = new int[3];
             for (int i = 0; i < 3; i++) {
                 martiansInput[i] = sc.nextInt();
             }
-            
 
             for (int i = 0; i < 3; i++) {
                 if (cargoLocation[martiansInput[i] - 1] > 0) {
-                    ans.add(martiansInput[i]);
+                    for (int j = 0; j < 7; j++) {
+                        if (cargoLocation[j] != 0) {
+                            for (int k = 0; k < 7; k++) {
+                                if (cargoLocation[k] == 0) {
+                                    cargoLocation[j] = cargoLocation[martiansInput[i] - 1];
+                                    cargoLocation[martiansInput[i] - 1] = 0;
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
             System.out.println(ans);
         }
 
+    }
+
+    public static void newCargoLocation(int[] cargoLocation) {
+        try (Scanner sc = new Scanner(System.in)) {
+            for (int i = 0; i < 7; i++) {
+                cargoLocation[i] = sc.nextInt();
+            }
+        }
     }
 
 }
