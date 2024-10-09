eaf2ec300fdab5091a313bde0e96d3ac0052fb0a
barhat <barhatbekuulubaktiyar@gmail.com>
2024-10-09 10:26:52 +0600

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,44 @@
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int[] cargoLocation = {0, 0, 311, 0, 17, 385, 0};
-            System.out.println("Do you want to enter cargo box locations by hand?"
-                    + "If so, please enter 1, otherwise 0");
-            int flag = sc.nextInt();
-            if (flag == 1)
-                newCargoLocation(cargoLocation);
+        Scanner sc = new Scanner(System.in);
+        int[] cargoLocation = {0, 0, 311, 0, 17, 385, 0};
+        System.out.println("Do you want to enter cargo box locations by hand?"
+                + "If so, please enter 1, otherwise 0");
+        int flag = sc.nextInt();
+        if (flag == 1) {
+            newCargoLocation(cargoLocation);
+        }
 
-            ArrayList<Integer> ans = new ArrayList<>();
-            int[] martiansInput = new int[3];
+        int[] martiansInput = new int[3];
+        for (int i = 0; i < 3; i++) {
+            martiansInput[i] = sc.nextInt();
+        }
+        int count = correctGuessCount(cargoLocation, martiansInput);
+        while (count < 3) {
             for (int i = 0; i < 3; i++) {
                 martiansInput[i] = sc.nextInt();
             }
-
-            for (int i = 0; i < 3; i++) {
-                if (cargoLocation[martiansInput[i] - 1] > 0) {
-                    for (int j = 0; j < 7; j++) {
-                        if (cargoLocation[j] != 0) {
-                            for (int k = 0; k < 7; k++) {
-                                if (cargoLocation[k] == 0) {
-                                    cargoLocation[j] = cargoLocation[martiansInput[i] - 1];
-                                    cargoLocation[martiansInput[i] - 1] = 0;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            System.out.println(ans);
+            count = correctGuessCount(cargoLocation, martiansInput);
         }
 
+        for (int i = 0; i < 3; i++) {
+            System.out.print(martiansInput[i] + " ");
+        }
+    }
+
+    public static int correctGuessCount(int[] cargoLocation, int[] martiansInput) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (cargoLocation[martiansInput[i] - 1] > 0) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void newCargoLocation(int[] cargoLocation) {
